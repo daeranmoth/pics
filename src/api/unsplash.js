6aa1544530c55e334769a8f317d6d8ca549bd217
gary camboulives <gary.camboulives@gmail.com>
2022-11-29 19:37:39 +0100

    hooksaregood

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 
-export default axios.create({
-  baseURL: 'https://api.unsplash.com',
-  headers: {
-        Authorization: 'Client-ID rFaUDtVGkvZRed6M5NA6-UVMl9rLTeJyjb0UmcMuwr4'
-      }
-});
+const searchImages = async (term) => {
+  const response = await axios.get("https://api.unsplash.com/search/photos", {
+    headers: {
+      Authorization: "Client-ID 8O50V7bNzfKdVixwS9W9nZVdr0VnrCv9gmeimfdvp6Y",
+    },
+    params: {
+      query: term,
+    },
+  });
+
+  return response.data.results;
+};
+
+export default searchImages;
